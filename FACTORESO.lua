99c31dfa2db06bda310556be2088d2aeac46983f
Carson Williams <sxhatrix@gmail.com>
2018-09-02 14:20:38 -0600

    saved var stuff

--- FACTORESO.lua
+++ FACTORESO.lua
@@ -2,6 +2,8 @@ FACTORESO = {}
 FACTORESO.name = "FACTORESO"
 
 function FACTORESO:Initialize()
+    self.savedVariables = ZO_SavedVars:New("FACTORESOSavedVariables", 1, nil, {})
+    self:RestorePosition()
 end
 
 function FACTORESO.OnAddOnLoaded(event, addonName)
@@ -9,5 +11,18 @@ function FACTORESO.OnAddOnLoaded(event, addonName)
         FACTORESO:Initialize()
     end
 end
+
+function FACTORESO.OnIndicatorMoveStop()
+    FACTORESO.savedVariables.left = FACTORESOIndicator:GetLeft()
+    FACTORESO.savedVariables.top = FACTORESOIndicator:GetTop()
+end
+
+function FACTORESO:RestorePosition()
+    local left = self.savedVariables.left
+    local top = self.savedVariables.top
+   
+    FACTORESOIndicator:ClearAnchors()
+    FACTORESOIndicator:SetAnchor(TOPLEFT, GuiRoot, TOPLEFT, left, top)
+  end
 
 EVENT_MANAGER:RegisterForEvent(FACTORESO.name, EVENT_ADD_ON_LOADED, FACTORESO.OnAddOnLoaded)
