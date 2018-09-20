7c7ad5eaf36a920a5d9cefaa40881b6c50763b45
Carson Williams <sxhatrix@gmail.com>
2018-09-20 01:45:45 -0600

    adds some inefficient logic

--- FACTORESO.lua
+++ FACTORESO.lua
@@ -30,20 +30,90 @@ function FACTORESO.OnAddOnLoaded(event, addonName)
     end
 end
 
+function FACTORESO.ProcessDamageDone(damage, type, abilityId)
+end
+
+function FACTORESO.ProcessDamageTaken(damage, type, abilityId)
+end
 
 function FACTORESO.OnCombat(eventCode, result, isError, abilityName, abilityGraphic, 
 abilityActionSlotType, sourceName, sourceType, targetName, targetType, hitValue, 
 powerType, damageType, log, sourceUnitId, targetUnitId, abilityId)
+    local target = zo_strformat("<<1>>", targetName)
+    local source = zo_strformat("<<1>>", sourceName)
 
-    if targetName ~= nil and zo_strformat("<<1>>", targetName) == FACTORESO.playerName 
-    and result == ACTION_RESULT_DAMAGE and damageType ~= nil and hitValue ~= nil then
-        FACTORESO.damages[damageType] = FACTORESO.damages[damageType] + hitValue
-        --d(zo_strformat("i took <<1>> damage of damagetype=<<2>>. total=<<3>>", 
-        --  hitValue, damageType, FACTORESO.damages[damageType]))
+    if result == ACTION_RESULT_DAMAGE then
+        if target == FACTORESO.playerName then
+            --d("i took dmg")
+            FACTORESO.damages[damageType] = FACTORESO.damages[damageType] + hitValue
+        elseif source == FACTORESO.playerName then
+            --d("i did dmg")
+        end
+    elseif result == ACTION_RESULT_CRITICAL_DAMAGE then
+        if target == FACTORESO.playerName then
+            --d("i took crit dmg")
+        elseif source == FACTORESO.playerName then
+            --d("i did crit dmg")
+        end
+    elseif result == ACTION_RESULT_DOT_TICK then
+        if target == FACTORESO.playerName then
+            --d("i took dot dmg")
+        elseif source == FACTORESO.playerName then
+            --d("i did dot dmg")
+        end
+    elseif result == ACTION_RESULT_DOT_TICK_CRITICAL then
+        if target == FACTORESO.playerName then
+            --d("i took crit dot dmg")
+        elseif source == FACTORESO.playerName then
+            --d("i did crit dot dmg")
+        end
+    elseif result == ACTION_RESULT_HEAL then
+        if target == FACTORESO.playerName then
+            --d("i was healed")
+        elseif source == FACTORESO.playerName then
+            --d("i healed something")
+        end
+    elseif result == ACTION_RESULT_CRITICAL_HEAL then
+        if target == FACTORESO.playerName then
+            --d("i was crit healed")
+        elseif source == FACTORESO.playerName then
+            --d("i crit healed something")
+        end
+    elseif result == ACTION_RESULT_HOT_TICK then
+        if target == FACTORESO.playerName then
+            --d("i was heal ticked")
+        elseif source == FACTORESO.playerName then
+            --d("i heal ticked something")
+        end
+    elseif result == ACTION_RESULT_HOT_TICK_CRITICAL then
+        if target == FACTORESO.playerName then
+            --d("i was crit heal ticked")
+        elseif source == FACTORESO.playerName then
+            --d("i crit heal ticked something")
+        end
     end
 end
 
 function FACTORESO.ShowMain()
+    local totalDamage = 
+    FACTORESO.damages[DAMAGE_TYPE_COLD] +
+    FACTORESO.damages[DAMAGE_TYPE_DISEASE] +
+    FACTORESO.damages[DAMAGE_TYPE_DROWN] +
+    FACTORESO.damages[DAMAGE_TYPE_EARTH] +
+    FACTORESO.damages[DAMAGE_TYPE_FIRE] +
+    FACTORESO.damages[DAMAGE_TYPE_GENERIC] +
+    FACTORESO.damages[DAMAGE_TYPE_MAGIC] +
+    FACTORESO.damages[DAMAGE_TYPE_NONE] +
+    FACTORESO.damages[DAMAGE_TYPE_OBLIVION] +
+    FACTORESO.damages[DAMAGE_TYPE_PHYSICAL] +
+    FACTORESO.damages[DAMAGE_TYPE_POISON] +
+    FACTORESO.damages[DAMAGE_TYPE_SHOCK]
+
+    local percentFire = (FACTORESO.damages[DAMAGE_TYPE_FIRE] / totalDamage) * 100
+
+    FACTORESO_MainLabel:SetText(zo_strformat(
+        "total damage taken: <<1>>\n % fire damage: <<2>>", totalDamage, percentFire
+    ));
     SCENE_MANAGER:ToggleTopLevel(FACTORESO_Main)
 end
 
